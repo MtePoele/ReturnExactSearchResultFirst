a588ad112004500bb4cba49cb51e8d61fe0e6b06
Mark te Poele <mark.tepoele@itvitaelearning.nl>
2024-02-09 14:16:16 +0100

    Removed unnessecary ToLower()

--- ReturnExactSearchResultFirst/Program.cs
+++ ReturnExactSearchResultFirst/Program.cs
@@ -11,22 +11,22 @@ List<Company> returnedFromDatabase = new()
     new Company { Id = 6, City = "Broekhuizen(DR)"},
     new Company { Id = 7, City = "Broekhuizen(LB)"},
     new Company { Id = 8, City = "Broekhuizenvorst"},
-    new Company { Id = 9, City = "Biddinghuizen"},
+    new Company { Id = 9, City = "almere"},
     new Company { Id = 10, City = "Delfstrahuizen"},
-    new Company { Id = 11, City = "Huizen"},
+    new Company { Id = 11, City = "almelo"},
 };
 
 // user input, voor dit voorbeeld hardcoded
-string keyword = "huiz";
+string keyword = "alm";
 
 // sorteert door namen die beginnen met de user input aan het begin te zetten, de rest daarna alfabetisch
 List<Company> sortedresult = returnedFromDatabase
     .OrderByDescending(c => c.City.ToLower().StartsWith(keyword.ToLower()))
-    .ThenBy(c => c.City.ToLower())
+    .ThenBy(c => c.City)
     .ToList();
 
 // dit is een console app, zo kan ik de eerste 10 printen
-for (int count = 0; count < 10; count++)
+for (int count = 0; count < 2; count++)
 {
     Console.WriteLine(sortedresult[count].City);
 }
